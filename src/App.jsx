f9a984978037d03a4f2d02edb21ed4ec1334d114
Raoul Daou <raoul@Raouls-MacBook-Pro.local>
2024-09-25 00:08:56 +0200

    export app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
 
 const App = () => {
   return (
@@ -10,6 +9,4 @@ const App = () => {
   );
 };
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+export default App;
