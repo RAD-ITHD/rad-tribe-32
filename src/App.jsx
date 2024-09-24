710ca96bea83e23871e72e90a09666340485fdd3
Raoul Daou <raoul@Raouls-MacBook-Pro.local>
2024-09-25 00:24:29 +0200

    Add a potato

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import potatoImage from "./assets/potato.jpg";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello, React!</h1>
-      <p>This is a simple test component.</p>
+      <h1>Hello!</h1>
+      <p>This is a simple picture of a potato.</p>
+      <img src={potatoImage} alt="A potato" style={{ width: "300px", height: "auto" }} />
     </div>
   );
 };
